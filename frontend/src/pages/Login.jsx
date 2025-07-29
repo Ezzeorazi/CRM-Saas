aa1b1fce44865daad568ba7c9c1e69aee0807d91
Ezzeorazi <ezequiel.orazi90@gmail.com>
2025-07-29 04:51:40 -0300

    Changes env2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
     setError('');
 
     try {
-      const { data } = await axios.post('http://localhost:5000/api/auth/login', {
+      const { data } = await axios.post(`${API_URL}/auth/login`, {
         email,
         contraseña
       });
