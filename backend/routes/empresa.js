d533b2048f144d02856ee3f87f70e6dad445040d
Ezzeorazi <ezequiel.orazi90@gmail.com>
2025-08-01 14:03:38 -0300

    Detele Backend

--- backend/routes/empresa.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { subirLogo, uploadLogo } = require('../controllers/empresaController');
-const { verificarToken } = require('../middleware/authMiddleware');
-
-router.post('/logo', verificarToken, uploadLogo.single('logo'), subirLogo);
-
-module.exports = router;
